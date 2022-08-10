14f7b1f5e7ce49ab8a18de19edf304011ca72432
TanyaGr9191 <tania91.gr@gmail.com>
2022-08-10 17:29:47 +0300

    changed img path

--- js/memeService.js
+++ js/memeService.js
@@ -6,10 +6,10 @@ var gKeywordSearchCountMap = {
 
 var gImgs = [
     { id: 1, url: '../DESIGN/meme-imgs(square)/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: '../DESIGN//meme-imgs(square)/2.jpg', keywords: ['dog', 'happy'] },
-    { id: 3, url: '../DESIGN//meme-imgs(square)/3.jpg', keywords: ['baby', 'dog'] },
-    { id: 4, url: '../DESIGN//meme-imgs(square)/4.jpg', keywords: ['cat', 'sleep'] },
-    { id: 5, url: '../DESIGN//meme-imgs(square)/5.jpg', keywords: ['baby', 'success'] },
+    { id: 2, url: '../DESIGN/meme-imgs(square)/2.jpg', keywords: ['dog', 'happy'] },
+    { id: 3, url: '../DESIGN/meme-imgs(square)/3.jpg', keywords: ['baby', 'dog'] },
+    { id: 4, url: '../DESIGN/meme-imgs(square)/4.jpg', keywords: ['cat', 'sleep'] },
+    { id: 5, url: '../DESIGN/meme-imgs(square)/5.jpg', keywords: ['baby', 'success'] },
 ]
 
 var gMeme = {
