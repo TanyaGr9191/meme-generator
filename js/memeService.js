f3cd92396854b7aecb6aae0fb02009dd27e96c66
TanyaGr9191 <tania91.gr@gmail.com>
2022-08-10 17:44:59 +0300

    changed img folder

--- js/memeService.js
+++ js/memeService.js
@@ -5,11 +5,11 @@ var gKeywordSearchCountMap = {
 }
 
 var gImgs = [
-    { id: 1, url: '../DESIGN/meme-imgs(square)/1.jpg', keywords: ['funny', 'cat'] },
-    { id: 2, url: '../DESIGN/meme-imgs(square)/2.jpg', keywords: ['dog', 'happy'] },
-    { id: 3, url: '../DESIGN/meme-imgs(square)/3.jpg', keywords: ['baby', 'dog'] },
-    { id: 4, url: '../DESIGN/meme-imgs(square)/4.jpg', keywords: ['cat', 'sleep'] },
-    { id: 5, url: '../DESIGN/meme-imgs(square)/5.jpg', keywords: ['baby', 'success'] },
+    { id: 1, url: '../img/1.jpg', keywords: ['funny', 'cat'] },
+    { id: 2, url: '../img/2.jpg', keywords: ['dog', 'happy'] },
+    { id: 3, url: '../img/3.jpg', keywords: ['baby', 'dog'] },
+    { id: 4, url: '../img/4.jpg', keywords: ['cat', 'sleep'] },
+    { id: 5, url: '../img/5.jpg', keywords: ['baby', 'success'] },
 ]
 
 var gMeme = {
